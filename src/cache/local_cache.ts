c259a89754bb73ca4140b559679a46385f8d75b6
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-02 11:31:11 -0300

    :recycle: update: fun - isExpired

--- src/cache/local_cache.ts
+++ src/cache/local_cache.ts
@@ -1,3 +1,9 @@
 const CACHE = new Map<string, any>()
 
-export const isExpired = (key: string) => new Date() > CACHE.get(key).expire;
+export const isExpired = (key: string) => {
+    const expired = new Date() > CACHE.get(key).expire;
+
+    if (expired) remove(key)
+
+    return expired;
+}
