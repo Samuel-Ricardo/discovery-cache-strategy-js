6ef5687897c8ee344cfecf8201487328c419ff31
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-02 09:41:01 -0300

    :gear: create: const - REDIS_ENABLED

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -2,6 +2,6 @@ const ENV = process.env;
 
 export const VIA_CEP_URI = (cep: string) => `https://viacep.com.br/ws${cep}/json/`
 
-export const TIME_TO_LEAVE = ENV.TTL;
-
+export const TIME_TO_LEAVE = ENV.TTL ? ENV.TTL : 60;
 export const CACHE_SCHEDULER = ENV.CACHE_SCHEDULER; 
+export const REDIS_ENABLED = ENV.REDIS_ENABLED ? ENV.REDIS_ENABLED : false;
