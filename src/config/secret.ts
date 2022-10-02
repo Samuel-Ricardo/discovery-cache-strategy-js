152c7f694d64143755f0b4cd495b068a12084f1f
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-02 09:37:49 -0300

    :gear: create: const - CACHE_SCHEDULER

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -3,3 +3,5 @@ const ENV = process.env;
 export const VIA_CEP_URI = (cep: string) => `https://viacep.com.br/ws${cep}/json/`
 
 export const TIME_TO_LEAVE = ENV.TTL;
+
+export const CACHE_SCHEDULER = ENV.CACHE_SCHEDULER; 
