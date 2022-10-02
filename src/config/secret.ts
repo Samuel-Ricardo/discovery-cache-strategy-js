4b3482bf9b3bc6ffb0f20c635a34eb45ab449ee1
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-02 09:41:15 -0300

    :gear: create: const - REDIS_URL

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -5,3 +5,4 @@ export const VIA_CEP_URI = (cep: string) => `https://viacep.com.br/ws${cep}/json
 export const TIME_TO_LEAVE = ENV.TTL ? ENV.TTL : 60;
 export const CACHE_SCHEDULER = ENV.CACHE_SCHEDULER; 
 export const REDIS_ENABLED = ENV.REDIS_ENABLED ? ENV.REDIS_ENABLED : false;
+export const REDIS_URL = ENV.REDIS_URL ? ENV.REDIS_URL : 'localhost:6379';
