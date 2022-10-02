b112ca6ac2a0f18eb18dd220c9ad3b4ac31a6eb1
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-02 10:55:05 -0300

    :sparkles: create: fun - findByCep

---
+++ src/client/via_cep_client.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { ICep } from "@type"
+import { VIA_CEP_URI } from "../config/secret"
+
+export const findByCep = async (cep:string) => {
+    console.info(`Fiding CEP data from external API - [${cep}]`)
+
+    let cep_result:ICep | null = null;
+
+    await axios.get(VIA_CEP_URI(cep)).
+        then((response) => {
+            cep_result = {...response.data}
+        }).
+        catch((err) => {
+            console.error(`Error on call ViaCep API to CEP - [ ${cep} ]`);
+            console.error(err);
+        })
+    return cep_result;
+}
