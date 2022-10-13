937132c70caa007ed4730a9b3902f76920da2b8f
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-12 22:53:46 -0300

    :sparkles: update: local_cache

--- src/cache/local_cache.ts
+++ src/cache/local_cache.ts
@@ -1,33 +1,33 @@
-import { ICacheItem } from "@type"
+import { ICacheItem, ICache } from "@type"
 import { TIME_TO_LEAVE } from "config/secret"
 
-const CACHE = new Map<string, ICacheItem<any>>()
+export const LOCAL_CACHE = new Map<string, ICacheItem<any>>()
 
 //const sync = (cache:Map<string, ICache<any>>) => CACHE = cache;
 
-export const save = (key: string, value: any, override?: boolean) => {
+ const save = (key: string, value: any, override?: boolean) => {
     
     const cache:ICacheItem<any> = {
         value,
         expire: getExpirationTime()
     }
 
-    if(exists(key)) return override? CACHE.set(key, cache) : null
+    if(exists(key)) return override? LOCAL_CACHE.set(key, cache) : null
     
-    CACHE.set(key, cache);
+    LOCAL_CACHE.set(key, cache);
 }
 
 
-export const get = (key: string) => isAble(key)? CACHE.get(key) : null 
+ const get = (key: string) => isAble(key)? LOCAL_CACHE.get(key) : null 
 
-export const exists = (key: string) => CACHE.has(key) 
+ const exists = (key: string) => LOCAL_CACHE.has(key) 
 
-export const remove = (key: string) => CACHE.delete(key)
+ const remove = (key: string) => LOCAL_CACHE.delete(key)
 
-export const isAble = (key: string) => !isExpired(key) && exists(key)
+ const isAble = (key: string) => !isExpired(key) && exists(key)
 
-export const isExpired = (key: string) => {
-    const data = CACHE.get(key);
+ const isExpired = (key: string) => {
+    const data = LOCAL_CACHE.get(key);
     if(!data) return true;
 
     const expired = new Date() > data.expire
@@ -42,6 +42,8 @@ function getExpirationTime(): Date {
     return now;
 }
 
-export const removeExpiredKeys = () => Array.from(CACHE.keys())
+ const removeExpiredKeys = () => Array.from(LOCAL_CACHE.keys())
                                             .filter(key => isExpired(key))
                                             .forEach(key => remove(key))
+ 
+                                            
