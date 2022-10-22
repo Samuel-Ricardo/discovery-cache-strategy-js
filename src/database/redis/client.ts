1b2b0b738aff8e1d231db0a94793fe903413fb73
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-22 18:39:49 -0300

    :goal_net: create: handler - redis (client_error)

--- src/database/redis/client.ts
+++ src/database/redis/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@redis/client";
 import { REDIS_URL } from "config/secret";
 
-const redis = createClient({
-    url: `redis://${REDIS_URL}`
-})
+const redis = createClient({url: `redis://${REDIS_URL}`})
+
+redis.on("error", (error) => console.error("Redis Client error", error))
