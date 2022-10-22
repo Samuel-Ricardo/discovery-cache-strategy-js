861eda2e607e96d5d3dd06b97cc8920e1b8508a7
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-22 19:01:29 -0300

    :goal_net: catch: error - save (redis)

--- src/database/redis/operations.ts
+++ src/database/redis/operations.ts
@@ -2,7 +2,9 @@ import { TIME_TO_LEAVE } from "config/secret"
 import { redis } from "./client"
 
 export const save = async (key: any, value: any) => {
-    try {
-        await redis.set(key, value, {EX: TIME_TO_LEAVE})
-    }
+    
+    try { await redis.set(key, value, {EX: TIME_TO_LEAVE}) }
+    
+    catch (error){ console.error("Erro on try to save data in Redis", error) }
 }
+
