9a9f9018d7ad860c49dfc7251eb43c0619914216
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-23 16:16:26 -0300

    :recycle: refact

--- src/database/redis/operations.ts
+++ src/database/redis/operations.ts
@@ -3,8 +3,9 @@ import { redis } from "./client"
 
 export const save = async (key: any, value: any) => {
     
-    try { await redis.set(key, value, {EX: TIME_TO_LEAVE}) }
+    try { return await redis.set(key, value, {EX: TIME_TO_LEAVE}) }
     
     catch (error){ console.error("Erro on try to save data in Redis", error) }
 }
 
+export const get = async (key: any) => await redis.get(key);
