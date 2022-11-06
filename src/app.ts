3c91cca0dd32aa19fac1a2e1001a24edcb45edb3
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-11-06 14:39:39 -0300

    :gear: update: app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express"
+import router from "routes/app_routes";
 import { PORT } from "./config/secret";
+import {expiredCacheCleaner} from "./jobs/cache_cleaner"
 
 const app = express();
 
+expiredCacheCleaner();
+
+app.use(router)
 app.listen(PORT, () => { console.info(`Application started at port ${PORT} successfully!`) });
