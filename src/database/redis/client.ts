ec3b8e2a67d5faf6b87e070192bf03152261edff
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-11-06 11:26:02 -0300

    :card_file_box: connect: database - redis

--- src/database/redis/client.ts
+++ src/database/redis/client.ts
@@ -4,3 +4,7 @@ import { REDIS_URL } from "config/secret";
 const redis = createClient({url: `redis://${REDIS_URL}`})
 
 redis.on("error", (error) => console.error("Redis Client error", error))
+
+redis.connect();
+
+export {redis};
