c3744fc8ba12e046c6b98f3af01bc3abbf31bcc2
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-11-06 12:28:28 -0300

    :recycle: refact: fun - removeKeys (jobs)

--- src/jobs/cache_cleaner.ts
+++ src/jobs/cache_cleaner.ts
@@ -2,4 +2,4 @@ import { local_cache_actions as _ } from 'cache/local_cache'
 import { CACHE_SCHEDULER } from 'config/secret'
 import schedule from 'node-schedule'
 
-export const expiredCacheCleaner = () => schedule.scheduleJob(CACHE_SCHEDULER, () => _.removeExpiredKeys())
+export const expiredCacheCleaner = () => schedule.scheduleJob(CACHE_SCHEDULER, () => _.removeExpiredKeys!())
